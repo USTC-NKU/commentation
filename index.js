8f7288baada69a2a80ce2e236a67dd6c9919365f
USTC-NKU <90564000+USTC-NKU@users.noreply.github.com>
2022-03-16 01:13:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,168 +1,40 @@
-const Waline = require('@waline/vercel');
+const Application = require('@waline/vercel');
 
-module.exports = Waline({
-    forbiddenWords: [
-        '习近平',
-        '毛泽东',
-        '快递',
-        '空包',
-        '代发'
-    ],
-    disallowIPList: [
-        '220.173.125.83'
-    ],
-    secureDomains: [
-        'waline.js.org',
-    ],
-    mailSubject: '{{parent.nick}}，您在 {{site.name}} 上发表的评论,收到了来自 {{self.nick}} 的回复',
-    mailSubjectAdmin: '{{site.name}} 上收到了来自 {{self.nick}} 的留言',
-    mailTemplate: '<head><base target="_blank"/><style id="scrollbar"type="text/css">::-webkit-scrollbar{width:0!important}pre{white-space:pre-wrap!important;word-wrap:break-word!important;*white-space:normal!important}pre{white-space:pre-wrap!important;word-wrap:break-word!important;*white-space:normal!important}#letter img{max-width:300px}</style><style id="from-wrapstyle"type="text/css">#form-wrap{overflow:hidden;height:447px;position:relative;top:0px;transition:all 1s ease-in-out.3s;z-index:0}</style><style id="from-wraphoverstyle"type="text/css">#form-wrap:hover{height:1300px;top:-200px}</style></head><body><div style="width: 530px;margin: 20px auto 0;height: 1000px;"><div id="form-wrap"><img src="https://cdn.jsdelivr.net/gh/Akilarlxh/Valine-Admin@v1.0/source/img/before.png"alt="before"style="position: absolute;bottom: 126px;left: 0px;background-repeat: no-repeat;width: 530px;height: 317px;z-index:-100"><div style="position: relative;overflow: visible;height: 1500px;width: 500px;margin: 0px auto;transition: all 1s ease-in-out .3s;padding-top:200px;"<form><div style="background: white;width: 95%;max-width: 800px;margin: auto auto;border-radius: 5px;border: 1px solid;overflow: hidden;-webkit-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.12);box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.18);"><img style="width:100%;overflow: hidden;"src="https://area.sinaapp.com/bingImg"/><div style="padding: 5px 20px;"><br><div><h3 style="text-decoration: none; color: #8fbc8f;">{{parent.nick}}，见信安：</h3></div><br><div id="letter"style="overflow:auto;height:300px;width:100%;display:block;word-break: break-all;word-wrap: break-word;"><p style="display: inline-block;">您在<a style="text-decoration: none;color: #8fbc8f"target="_blank"href="{{site.postUrl}} display: inline-block;">{{site.name}}</a>上发表的评论:</p><div style="border-bottom: #ddd 1px solid;border-left: #ddd 1px solid;padding-bottom: 20px;background-color: #eee;margin: 15px 0px;padding-left: 20px;padding-right: 20px;border-top: #ddd 1px solid;border-right: #ddd 1px solid;padding-top: 20px;font-family: "Arial", "Microsoft YaHei" , "黑体" , "宋体" , sans-serif;">{{parent.comment | safe}}</div><p>收到了来自{{self.nick}}的回复：</p><div style="border-bottom: #ddd 1px solid;border-left: #ddd 1px solid;padding-bottom: 20px;background-color: #eee;margin: 15px 0px;padding-left: 20px;padding-right: 20px;border-top: #ddd 1px solid;border-right: #ddd 1px solid;padding-top: 20px;font-family: "Arial", "Microsoft YaHei" , "黑体" , "宋体" , sans-serif;">{{self.comment | safe}}</div></div><br><div style="text-align: center;margin-top: 40px;"><img src="https://ae01.alicdn.com/kf/U0968ee80fd5c4f05a02bdda9709b041eE.png"alt="hr"style="width:100%; margin:5px auto 5px auto; display: block;"/><a style="text-transform: uppercase;text-decoration: none;font-size: 14px;border: 2px solid #ace0f9;color: #FFFFFF;padding: 10px;display: inline-block;margin: 10px auto 0;background-color: #8fbc8f;"target="_blank"href="{{site.postUrl}}">{{site.name}}｜请您签收~</a></div><p style="font-size: 12px;text-align: center;color: #999;">自动书记人偶竭诚为您服务！<br>©2020 - 2021 <a style="text-decoration:none; color:#8fbc8f"href="{{site.url}}">{{site.name}}</a></p></div></div></form></div><img src="https://cdn.jsdelivr.net/gh/Akilarlxh/Valine-Admin@v1.0/source/img/after.png"alt="after"style="      position: absolute;bottom: -2px;left: 0;background-repeat: no-repeat;width: 530px;height: 259px;z-index:100"></div></div></body>',
-    mailTemplateAdmin: '<head><base target="_blank"/><style id="scrollbar"type="text/css">::-webkit-scrollbar{width:0!important}pre{white-space:pre-wrap!important;word-wrap:break-word!important;*white-space:normal!important}#letter img{max-width:300px}</style><style id="from-wrapstyle"type="text/css">#form-wrap{overflow:hidden;height:447px;position:relative;top:0px;transition:all 1s ease-in-out.3s;z-index:0}</style><style id="from-wraphoverstyle"type="text/css">#form-wrap:hover{height:1300px;top:-200px}</style></head><body><div style="width: 530px;margin: 20px auto 0;height: 1000px;"><div id="form-wrap"><img src="https://cdn.jsdelivr.net/gh/Akilarlxh/Valine-Admin@v1.0/source/img/before.png"alt="before"style="position: absolute;bottom: 126px;left: 0px;background-repeat: no-repeat;width: 530px;height: 317px;z-index:-100"><div style="position: relative;overflow: visible;height: 1500px;width: 500px;margin: 0px auto;transition: all 1s ease-in-out .3s;padding-top:200px;"><form><div style="background: white;width: 95%;max-width: 800px;margin: auto auto;border-radius: 5px;border: 1px solid;overflow: hidden;-webkit-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.12);box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.18);"><img style="width:100%;overflow: hidden;"src="https://area.sinaapp.com/bingImg"/><div style="padding: 5px 20px;"><br><div><h3 style="text-decoration: none; color: #8fbc8f;">来自{{self.nick}}的留言:</h3></div><br><br><div id="letter"style="overflow:auto;height:300px;width:100%;display:block;word-break: break-all;word-wrap: break-word;"><div style="border-bottom: #ddd 1px solid;border-left: #ddd 1px solid;padding-bottom: 20px;background-color: #eee;margin: 15px 0px;padding-left: 20px;padding-right: 20px;border-top: #ddd 1px solid;border-right: #ddd 1px solid;padding-top: 20px;font-family: "Arial", "Microsoft YaHei" , "黑体" , "宋体" , sans-serif;">{{self.comment | safe}}</div></div><div style="text-align: center;margin-top: 40px;"><img src="https://ae01.alicdn.com/kf/U0968ee80fd5c4f05a02bdda9709b041eE.png"alt="hr"style="width:100%; margin:5px auto 5px auto; display: block;"/><a style="text-transform: uppercase;text-decoration: none;font-size: 14px;border: 2px solid #ace0f9;color: #FFFFFF;padding: 10px;display: inline-block;margin: 10px auto 0;background-color: #8fbc8f;"target="_blank"href="{{site.postUrl}}">{{site.name}}｜请您过目~</a></div><p style="font-size: 12px;text-align: center;color: #999;">©2020 - 2021 <a style="text-decoration:none; color:#8fbc8f"href="{{site.url}}">{{site.name}}</a></p></div></div></form></div><img src="https://cdn.jsdelivr.net/gh/Akilarlxh/Valine-Admin@v1.0/source/img/after.png"alt="after"style="      position: absolute;bottom: -2px;left: 0;background-repeat: no-repeat;width: 530px;height: 259px;z-index:100"></div></div></body>'
+module.exports = Application({
+  mailSubject: '{{parent.nick}}，您在博客「{{site.name}}」上的评论收到了回复',
+  mailTemplate: `
+    <div style="border-top:2px solid #12ADDB;box-shadow:0 1px 3px #AAAAAA;line-height:180%;padding:0 15px 12px;margin:50px auto;font-size:12px;">
+      <h2 style="border-bottom:1px solid #DDD;font-size:14px;font-weight:normal;padding:13px 0 10px 8px;">        
+        您在博客<a style="text-decoration:none;color: #12ADDB;" href="{{site.url}}" target="_blank">{{site.name}}</a>上的评论有了新的回复
+      </h2>
+      <div style="padding:0 12px 0 12px;margin-top:18px">
+		<p>{{parent.nick}}，您曾发表评论：</p>
+        <div style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0;word-wrap:break-word;">{{parent.comment | safe}}</div>
+        <p><strong>{{self.nick}}</strong> 回复说：</p>
+        <div style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0;word-wrap:break-word;">{{self.comment | safe}}</div>
+        <p>您可以点击<a style="text-decoration:none; color:#12addb" href="{{site.postUrl}}" target="_blank">前往原文查看完整的回复內容</a>，欢迎再次光临<a style="text-decoration:none; color:#12addb" href="{{site.url}}" target="_blank">{{site.name}}</a>。</p>
+        <br/>
+      </div>
+	  <div style="border-top:1px solid #DDD; padding:13px 0 0 8px;">
+		该邮件为系统自动发送的邮件，请勿直接回复。
+	  </div>
+	  <br/>
+    </div>`,
+  mailSubjectAdmin: '您的博客「{{site.name}}」收到了新评论',
+  mailTemplateAdmin: `
+    <div style="border-top:2px solid #12ADDB;box-shadow:0 1px 3px #AAAAAA;line-height:180%;padding:0 15px 12px;margin:50px auto;font-size:12px;">
+      <h2 style="border-bottom:1px solid #DDD;font-size:14px;font-weight:normal;padding:13px 0 10px 8px;">        
+        有人在<a style="text-decoration:none;color: #12ADDB;" href="{{site.url}}" target="_blank">{{site.name}}</a>发表了新的评论
+      </h2>
+      <div style="padding:0 12px 0 12px;margin-top:18px">
+		<p><strong>{{self.nick}}</strong> 评论说：</p>
+		<div style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0;word-wrap:break-word;">{{self.comment | safe}}</div>
+        <p>您可以点击<a style="text-decoration:none; color:#12addb" href="{{site.postUrl}}" target="_blank">前往原文查看完整的评论内容。</a></p>
+        <br/>
+      </div>
+	  <div style="border-top:1px solid #DDD; padding:13px 0 0 8px;">
+		该邮件为系统自动发送的邮件，请勿直接回复。
+	  </div>
+	  <br/>
+    </div>`
 });
-// const Application = require('@waline/vercel');
-
-// module.exports = Waline({
-//   forbiddenWords: [
-//         '习近平',
-//         '毛泽东',
-//         '快递',
-//         '空包',
-//         '代发'
-//     ],
-//   secureDomains: [
-//         'waline.js.org',
-//     ],
-//   mailSubject: '{{parent.nick}}，您在博客「{{site.name}}」上的评论收到了回复',
-//   mailTemplate: '
-//   <head>
-//     <base target="_blank" />
-//     <style id="scrollbar" type="text/css">
-//          ::-webkit-scrollbar {
-//             width: 0!important
-//         }
-        
-//         pre {
-//             white-space: pre-wrap!important;
-//             word-wrap: break-word!important;
-//             *white-space: normal!important
-//         }
-        
-//         pre {
-//             white-space: pre-wrap!important;
-//             word-wrap: break-word!important;
-//             *white-space: normal!important
-//         }
-        
-//         #letter img {
-//             max-width: 300px
-//         }
-//     </style>
-//     <style id="from-wrapstyle" type="text/css">
-//         #form-wrap {
-//             overflow: hidden;
-//             height: 447px;
-//             position: relative;
-//             top: 0px;
-//             transition: all 1s ease-in-out.3s;
-//             z-index: 0
-//         }
-//     </style>
-//     <style id="from-wraphoverstyle" type="text/css">
-//         #form-wrap:hover {
-//             height: 1300px;
-//             top: -200px
-//         }
-//     </style>
-// </head>
-
-// <body>
-//     <div style="width: 530px;margin: 20px auto 0;height: 1000px;">
-//         <div id="form-wrap"><img src="https://cdn.jsdelivr.net/gh/Akilarlxh/Valine-Admin@v1.0/source/img/before.png" alt="before" style="position: absolute;bottom: 126px;left: 0px;background-repeat: no-repeat;width: 530px;height: 317px;z-index:-100">
-//             <div style="position: relative;overflow: visible;height: 1500px;width: 500px;margin: 0px auto;transition: all 1s ease-in-out .3s;padding-top:200px;" <form>
-//                 <div style="background: white;width: 95%;max-width: 800px;margin: auto auto;border-radius: 5px;border: 1px solid;overflow: hidden;-webkit-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.12);box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.18);"><img style="width:100%;overflow: hidden;" src="https://area.sinaapp.com/bingImg" />
-//                     <div style="padding: 5px 20px;"><br>
-//                         <div>
-//                             <h3 style="text-decoration: none; color: #8fbc8f;">{{parent.nick}}，见信安：</h3>
-//                         </div><br>
-//                         <div id="letter" style="overflow:auto;height:300px;width:100%;display:block;word-break: break-all;word-wrap: break-word;">
-//                             <p style="display: inline-block;">您在<a style="text-decoration: none;color: #8fbc8f" target="_blank" href="{{site.postUrl}} display: inline-block;">{{site.name}}</a>上发表的评论:</p>
-//                             <div style="border-bottom: #ddd 1px solid;border-left: #ddd 1px solid;padding-bottom: 20px;background-color: #eee;margin: 15px 0px;padding-left: 20px;padding-right: 20px;border-top: #ddd 1px solid;border-right: #ddd 1px solid;padding-top: 20px;font-family: "
-//                                 Arial ", "Microsoft YaHei " , "黑体 " , "宋体 " , sans-serif;">{{parent.comment | safe}}</div>
-//                             <p>收到了来自{{self.nick}}的回复：</p>
-//                             <div style="border-bottom: #ddd 1px solid;border-left: #ddd 1px solid;padding-bottom: 20px;background-color: #eee;margin: 15px 0px;padding-left: 20px;padding-right: 20px;border-top: #ddd 1px solid;border-right: #ddd 1px solid;padding-top: 20px;font-family: "
-//                                 Arial ", "Microsoft YaHei " , "黑体 " , "宋体 " , sans-serif;">{{self.comment | safe}}</div>
-//                         </div><br>
-//                         <div style="text-align: center;margin-top: 40px;"><img src="https://ae01.alicdn.com/kf/U0968ee80fd5c4f05a02bdda9709b041eE.png" alt="hr" style="width:100%; margin:5px auto 5px auto; display: block;" /><a style="text-transform: uppercase;text-decoration: none;font-size: 14px;border: 2px solid #ace0f9;color: #FFFFFF;padding: 10px;display: inline-block;margin: 10px auto 0;background-color: #8fbc8f;"
-//                                 target="_blank" href="{{site.postUrl}}">{{site.name}}｜请您签收~</a></div>
-//                         <p style="font-size: 12px;text-align: center;color: #999;">自动书记人偶竭诚为您服务！<br>©2020 - 2021 <a style="text-decoration:none; color:#8fbc8f" href="{{site.url}}">{{site.name}}</a></p>
-//                     </div>
-//                 </div>
-//                 </form>
-//             </div><img src="https://cdn.jsdelivr.net/gh/Akilarlxh/Valine-Admin@v1.0/source/img/after.png" alt="after" style="      position: absolute;bottom: -2px;left: 0;background-repeat: no-repeat;width: 530px;height: 259px;z-index:100"></div>
-//     </div>
-// </body>
-// ',
-//   mailSubjectAdmin: '您的博客「{{site.name}}」收到了新评论',
-//   mailTemplateAdmin: '
-//     <head>
-//     <base target="_blank" />
-//     <style id="scrollbar" type="text/css">
-//          ::-webkit-scrollbar {
-//             width: 0!important
-//         }
-        
-//         pre {
-//             white-space: pre-wrap!important;
-//             word-wrap: break-word!important;
-//             *white-space: normal!important
-//         }
-        
-//         #letter img {
-//             max-width: 300px
-//         }
-//     </style>
-//     <style id="from-wrapstyle" type="text/css">
-//         #form-wrap {
-//             overflow: hidden;
-//             height: 447px;
-//             position: relative;
-//             top: 0px;
-//             transition: all 1s ease-in-out.3s;
-//             z-index: 0
-//         }
-//     </style>
-//     <style id="from-wraphoverstyle" type="text/css">
-//         #form-wrap:hover {
-//             height: 1300px;
-//             top: -200px
-//         }
-//     </style>
-// </head>
-// <body>
-//     <div style="width: 530px;margin: 20px auto 0;height: 1000px;">
-//         <div id="form-wrap"><img src="https://cdn.jsdelivr.net/gh/Akilarlxh/Valine-Admin@v1.0/source/img/before.png" alt="before" style="position: absolute;bottom: 126px;left: 0px;background-repeat: no-repeat;width: 530px;height: 317px;z-index:-100">
-//             <div style="position: relative;overflow: visible;height: 1500px;width: 500px;margin: 0px auto;transition: all 1s ease-in-out .3s;padding-top:200px;">
-//                 <form>
-//                     <div style="background: white;width: 95%;max-width: 800px;margin: auto auto;border-radius: 5px;border: 1px solid;overflow: hidden;-webkit-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.12);box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.18);"><img style="width:100%;overflow: hidden;" src="https://area.sinaapp.com/bingImg" />
-//                         <div style="padding: 5px 20px;"><br>
-//                             <div>
-//                                 <h3 style="text-decoration: none; color: #8fbc8f;">来自{{self.nick}}的留言:</h3>
-//                             </div><br><br>
-//                             <div id="letter" style="overflow:auto;height:300px;width:100%;display:block;word-break: break-all;word-wrap: break-word;">
-//                                 <div style="border-bottom: #ddd 1px solid;border-left: #ddd 1px solid;padding-bottom: 20px;background-color: #eee;margin: 15px 0px;padding-left: 20px;padding-right: 20px;border-top: #ddd 1px solid;border-right: #ddd 1px solid;padding-top: 20px;font-family: "
-//                                     Arial ", "Microsoft YaHei " , "黑体 " , "宋体 " , sans-serif;">{{self.comment | safe}}</div>
-//                             </div>
-//                             <div style="text-align: center;margin-top: 40px;"><img src="https://ae01.alicdn.com/kf/U0968ee80fd5c4f05a02bdda9709b041eE.png" alt="hr" style="width:100%; margin:5px auto 5px auto; display: block;" /><a style="text-transform: uppercase;text-decoration: none;font-size: 14px;border: 2px solid #ace0f9;color: #FFFFFF;padding: 10px;display: inline-block;margin: 10px auto 0;background-color: #8fbc8f;"
-//                                     target="_blank" href="{{site.postUrl}}">{{site.name}}｜请您过目~</a></div>
-//                             <p style="font-size: 12px;text-align: center;color: #999;">©2020 - 2021 <a style="text-decoration:none; color:#8fbc8f" href="{{site.url}}">{{site.name}}</a></p>
-//                         </div>
-//                     </div>
-//                 </form>
-//             </div><img src="https://cdn.jsdelivr.net/gh/Akilarlxh/Valine-Admin@v1.0/source/img/after.png" alt="after" style="      position: absolute;bottom: -2px;left: 0;background-repeat: no-repeat;width: 530px;height: 259px;z-index:100"></div>
-//     </div>
-// </body>
-// '
-// //   async postSave(comment) {
-// //     // do what ever you want after save comment
-// //   },
-// });
