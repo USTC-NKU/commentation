04df959182e36ac782e63cae3d055a7eb6e64812
USTC-NKU <90564000+USTC-NKU@users.noreply.github.com>
2022-03-27 21:15:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const Application = require('@waline/vercel');
 
 module.exports = Application({
   async postSave(comment) {
-    <p>Hi, {{username}}</p>
-<p>
-你在 {{appname}} 的评论收到了新的回复，请点击查看：
-</p>
-<p><a href="你的网址首页链接" style="display: inline-block; padding: 10px 20px; border-radius: 4px; background-color: #3090e4; color: #fff; text-decoration: none;">马上查看</a></p>
-
 
   },
 });
